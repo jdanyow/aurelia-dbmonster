d6e26bef05315ba2250fc19f062bbf52cf2b4f3c
Jeremy Danyow <jdanyow@gmail.com>
2015-11-27 10:10:51 -0500

    chore(package): add text dep

--- config.js
+++ config.js
@@ -19,6 +19,7 @@ System.config({
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
+    "text": "github:systemjs/plugin-text@0.0.3",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
